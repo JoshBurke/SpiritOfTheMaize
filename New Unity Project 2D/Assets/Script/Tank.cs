f3e756f001783aa6758eb596d840594446c63c5d
Ziang Wan <zawtom666@gmail.com>
2016-10-06 20:39:41 -0500

    Ziang Wan

---
+++ New Unity Project 2D/Assets/Script/Tank.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour {
+    private Rigidbody2D _rigidBody;
+    [SerializeField]
+    private float _moveSpeed = 3f;
+
+    private void Awake()
+    {
+
+    }
+    public void Drive(float forward)
+    {
+        _rigidBody.velocity = transform.up * forward * _moveSpeed;
+    }
+
+}
