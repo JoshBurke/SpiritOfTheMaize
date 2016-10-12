edb657dca06770b4a27ee690e406ec302972506e
jbelou3 <jbelou3@illinois.edu>
2016-10-12 14:37:41 -0500

    Initial Commit

---
+++ Spirit_of_the_Maize/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        coll.gameObject.transform.position = new Vector2(0, 1);
+        coll.gameObject.transform.localRotation = Quaternion.identity;
+    }
+
+}
---
+++ Spirit_of_the_Maize/Assets/Scripts/SimplePlatformController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A quick rundown of how to use this
+//First create an object, doesn't especially matter what
+//add a RigidBody2D, a BoxCollider2D, an Animator, and this script
+//also add a child camera and a child empty object called specifically "groundCheck"
+//Also make sure everything is on layer 'player'
+//For the animator, set controller to 'character', 
+//update mode to 'animate physics', and culling mode to 'cull update transforms'
+//As for the camera, make sure it's Z-cordinate is negative so it's far back enough to actually observe your object
+//groundCheck should be positioned below the object proper, it needs to be IN the ground while the object is on it
+//Now create some ground to stand on, this works with anything but I suggest you start with a cube
+//change the proportions to make a long and thing rectangle to serve as ground
+//change it's layer to ground and add a BoxCollider2D
+
+
+public class SimplePlatformController : MonoBehaviour {
+
+	[HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 750f;
+    public Transform groundCheck;
+
+    public bool grounded = false;
+    private Rigidbody2D rb2d;
+
+    
+    // Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        groundCheck = GameObject.Find("groundCheck").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if ((Input.GetKeyDown(KeyCode.Space) && grounded))
+            jump = true;
+
+	}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void Reset()
+    {
+        gameObject.transform.position = new Vector2(0, 1);
+    }
+}
