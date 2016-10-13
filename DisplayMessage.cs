3af38aa20019af4a6c5a24aee2d71ffc57bd5926
williamwang7 <william.wang.5047@gmail.com>
2016-10-12 20:21:45 -0500

    Add files via upload

---
+++ DisplayMessage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayMessage : MonoBehaviour
+{
+	public bool showText = false;
+	public Rect textArea = new Rect (0, 0, Screen.width, Screen.height);
+	public string message;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.E)) {
+			showText = !showText;
+		}
+	}
+
+	void OnGUI ()
+	{
+		GUIStyle center = GUI.skin.GetStyle ("Label");
+		center.alignment = TextAnchor.MiddleCenter;
+		if (showText) {			
+			GUI.Label (textArea, message, center);
+		}
+	}
+}
