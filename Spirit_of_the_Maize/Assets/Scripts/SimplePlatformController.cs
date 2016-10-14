fd915c9223443d3540a9fd3fcfc288b1c3641335
jbelou3 <jbelou3@illinois.edu>
2016-10-13 20:26:45 -0500

    Camera fixed and Comments added

--- Spirit_of_the_Maize/Assets/Scripts/SimplePlatformController.cs
+++ Spirit_of_the_Maize/Assets/Scripts/SimplePlatformController.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 //A quick rundown of how to use this
 //First create an object, doesn't especially matter what
-//add a RigidBody2D, a BoxCollider2D, an Animator, and this script
-//also add a child camera and a child empty object called specifically "groundCheck"
-//Also make sure everything is on layer 'player'
-//For the animator, set controller to 'character', 
-//update mode to 'animate physics', and culling mode to 'cull update transforms'
-//As for the camera, make sure it's Z-cordinate is negative so it's far back enough to actually observe your object
+//add a RigidBody2D, a BoxCollider2D, and this script
+//also add a child empty object called specifically "groundCheck"
+//Also make both everything is on layer 'player'
 //groundCheck should be positioned below the object proper, it needs to be IN the ground while the object is on it
 //Now create some ground to stand on, this works with anything but I suggest you start with a cube
 //change the proportions to make a long and thing rectangle to serve as ground
@@ -18,12 +15,12 @@ using System.Collections;
 public class SimplePlatformController : MonoBehaviour {
 
 	[HideInInspector] public bool facingRight = true;
-    [HideInInspector] public bool jump = false;
+    [HideInInspector] public bool jump = false; //set to true to make the player jump
 
-    public float moveForce = 365f;
-    public float maxSpeed = 5f;
-    public float jumpForce = 750f;
-    public Transform groundCheck;
+    public float moveForce = 365f; //how fast the player accelerates
+    public float maxSpeed = 5f; //how fast player can move
+    public float jumpForce = 750f; //how high player can jump
+    public Transform groundCheck; //located
 
     public bool grounded = false;
     private Rigidbody2D rb2d;
@@ -39,6 +36,7 @@ public class SimplePlatformController : MonoBehaviour {
 	void Update () {
 
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        //sees if any part of a line between the player and groundCheck is on the ground layer
 
         if ((Input.GetKeyDown(KeyCode.Space) && grounded))
             jump = true;
@@ -50,10 +48,10 @@ public class SimplePlatformController : MonoBehaviour {
         float h = Input.GetAxis("Horizontal");
 
         if (h * rb2d.velocity.x < maxSpeed)
-            rb2d.AddForce(Vector2.right * h * moveForce);
+            rb2d.AddForce(Vector2.right * h * moveForce); //actually moves the player
 
         if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y); //caps player speed
 
         if (h > 0 && !facingRight)
             Flip();
