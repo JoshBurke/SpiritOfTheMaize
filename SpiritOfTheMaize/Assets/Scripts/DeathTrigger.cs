9478e2d439d0eb041e9699d21ed73b4e94c84b9c
jbelou3 <jbelou3@illinois.edu>
2016-10-25 06:49:08 -0500

    did some commenting

--- SpiritOfTheMaize/Assets/Scripts/DeathTrigger.cs
+++ SpiritOfTheMaize/Assets/Scripts/DeathTrigger.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class DeathTrigger : MonoBehaviour {
 
-    //to use, make an object with a boxCollider2D, make it a trigger, and attach this script
+	public Vector2 startingPos = new Vector2(0,0);
+
+	//to use, make an object with a Collider2D, make it a trigger, and attach this script
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        coll.gameObject.transform.position = new Vector2(-15, 0); //resets position
+		coll.gameObject.transform.position = startingPos; //resets position
         coll.gameObject.transform.localRotation = Quaternion.identity; //resets rotation
+		//in case it was rotating for some strange reason, lock your damn z rotation in your rigidbody
     }
 
 }
--- SpiritOfTheMaize/Assets/Scripts/Patrol.cs
+++ SpiritOfTheMaize/Assets/Scripts/Patrol.cs
@@ -3,26 +3,28 @@ using System.Collections;
 
 public class Patrol : MonoBehaviour {
 
+	//don't forgot to add a rigidbody2d to the patroller
+
 	public GameObject patroller;
 	private Vector2 start;
 	public Vector2 end;
-	private float deltaX;
+	private float deltaX; //xy distances from start to end
 	private float deltaY;
-	private float lastX;
+	private float lastX; //hold the xy of either start or end, whichever was visited last
 	private float lastY;
-	public float speed = 2f;
+	public float speed; //speed, self explanatory
 	private Rigidbody2D rb2d;
 
 	void Start () {
-		patroller = gameObject;
-		start = new Vector2(patroller.transform.position.x,patroller.transform.position.y);
+		patroller = gameObject; //patroller = the object this script is attached to
+		start = new Vector2(patroller.transform.position.x,patroller.transform.position.y); //start is patroller's current position
 		rb2d = GetComponent<Rigidbody2D>();
 		deltaX = Mathf.Abs(end.x - start.x);
 		deltaY = Mathf.Abs(end.y - start.y);
 		lastX = patroller.transform.position.x;
 		lastY = patroller.transform.position.y;
-		start.Normalize();
-		end.Normalize();
+		start.Normalize(); //super secret tech, convets a vector2D to have magnitude 1, but keep it's proportions
+		end.Normalize(); //so it can be used to direct but not effect the speed
 		rb2d.velocity = new Vector2 (end.x * speed, end.y * speed);
 	}
 	
@@ -30,7 +32,7 @@ public class Patrol : MonoBehaviour {
 	void Update ()
 	{
 		if (Mathf.Abs (patroller.transform.position.x - lastX) >= deltaX || Mathf.Abs (patroller.transform.position.y - lastY) >= deltaY) {
-			rb2d.velocity = new Vector2 (-1 * rb2d.velocity.x, -1 * rb2d.velocity.y);
+			rb2d.velocity = new Vector2 (-1 * rb2d.velocity.x, -1 * rb2d.velocity.y); //reverses direction
 			lastX = patroller.transform.position.x;
 			lastY = patroller.transform.position.y;
 		}
